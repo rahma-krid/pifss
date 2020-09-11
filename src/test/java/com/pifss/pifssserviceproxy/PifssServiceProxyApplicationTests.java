6f010f3da93cd25a6977c86d6a38f2d21e8740c9
unknown <rahmakrid5@gmail.com>
2020-09-11 09:24:40 +0200

    registration

---
+++ src/test/java/com/pifss/pifssserviceproxy/PifssServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pifss.pifssserviceproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PifssServiceProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
